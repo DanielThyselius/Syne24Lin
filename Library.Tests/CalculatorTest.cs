9fab92b2889a00707254dceb70eb752fd24bdd5f
Daniel Thyselius <daniel@mindfulstack.se>
2025-01-27 12:02:34 +0100

    demo innan lunch

---
+++ Library.Tests/CalculatorTest.cs
@@ -0,0 +1,54 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void CanAddNumbers()
+        {
+            // Arrange 
+             var sut = new Calculator();
+
+            // Act
+            var actual = sut.Add(1, 2);
+
+            // Assert
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void CanSubtractNumbers()
+        {
+            // Arrange 
+            var sut = new Calculator();
+            var expected = 1;
+
+            // Act
+            var actual = sut.Subtract(3, 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanMultiplyFractions()
+        {
+            // Arrange 
+            var sut = new Calculator();
+            var expected = 6.4;
+
+            // Act
+            var actual = sut.Multiply(3.2f, 2);
+
+            // Assert
+            Assert.Equal(expected, actual, 5);
+        }
+
+    }
+}
