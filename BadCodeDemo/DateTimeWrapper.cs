57ab019b5770a26e166e1301d1a1e090ae67b208
Daniel Thyselius <daniel@mindfulstack.se>
2025-02-03 13:51:31 +0100

    Add assignments

---
+++ BadCodeDemo/DateTimeWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadCodeDemo
+{
+    public interface IDateTimeWrapper
+    {
+        DateTime Now { get; }
+    }
+
+    public class DateTimeWrapper : IDateTimeWrapper
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
