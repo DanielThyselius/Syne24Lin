c506b28fe23ae6c4c471f2f28d5e565b5ab25dbd
Daniel Thyselius <daniel@mindfulstack.se>
2025-02-18 12:08:54 +0100

    move endpoints into dedicated file

---
+++ BookStore.MinimalApi/Endpoints/BookEndpoints.cs
@@ -0,0 +1,58 @@
+﻿using BookStore.Lib;
+using BookStore.MinimalApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.MinimalApi.Endpoints
+{
+    public static class BookEndpoints
+    {
+        // models
+        record UpdateBookRequest(string Description, string Review);
+
+        // endpoints
+        public static void RegisterBookEndpoints(this IEndpointRouteBuilder app)
+        {
+            // Get All
+            app.MapGet("/api/books", (IBookService service) => service.Books);
+
+            // Get by Id
+            app.MapGet("/api/books/{id}", (int id, IBookService service) =>
+            {
+                return service.Books.Find(x => x.Id == id);
+            });
+
+            // Post
+            // TODO: use a DTO instead of Book
+            app.MapPost("api/books", (
+                [FromBody] Book book,
+                [FromServices] IBookService service) =>
+            {
+                var newId = service.Books.Max(x => x.Id) + 1;
+                book.Id = newId;
+                service.Books.Add(book);
+            });
+
+            // PATCH
+            app.MapPatch("api/books/{id}", (int id, UpdateBookRequest request, IBookService service) =>
+            {
+                var oldBook = service.Books.Find(x => x.Id == id);
+                if (oldBook is null)
+                    return Results.NotFound(id);
+
+                oldBook.Description = request.Description;
+                oldBook.Review = request.Review;
+
+                return Results.Ok();
+            });
+
+
+            // Delete
+            app.MapDelete("api/books/{id}", (int id, IBookService service) =>
+            {
+                var book = service.Books.Find(x => x.Id == id);
+
+                return service.Books.Remove(book) ? Results.NoContent() : Results.NotFound(id);
+            });
+        }
+    }
+}
--- BookStore.MinimalApi/Program.cs
+++ BookStore.MinimalApi/Program.cs
@@ -1,5 +1,6 @@
 
 using BookStore.Lib;
+using BookStore.MinimalApi.Endpoints;
 using BookStore.MinimalApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,50 +35,7 @@ namespace BookStore.MinimalApi
             app.UseAuthorization();
 
             // Map endpoints for books
-            // Get All
-            app.MapGet("/api/books", (IBookService service) => service.Books);
-
-            // Get by Id
-            app.MapGet("/api/books/{id}", (int id, IBookService service) =>
-            {
-                return service.Books.Find(x => x.Id == id);
-            });
-
-            // Post
-            app.MapPost("api/books", (
-                [FromBody] Book book,
-                [FromServices] IBookService service) =>
-            {
-                var newId = service.Books.Max(x => x.Id) + 1;
-                book.Id = newId;
-                service.Books.Add(book);
-            });
-
-            // Put
-            app.MapPut("api/books/{id}", (int id, Book book, IBookService service) =>
-            {
-                var oldBook = service.Books.Find(x => x.Id == id);
-                if (oldBook is null)
-                    return Results.NotFound(id);
-
-                book.Id = id; // avoid changing Ids, could be handled better
-                oldBook.Title = book.Title;
-                oldBook.Author = book.Author;
-                oldBook.Description = book.Description;
-                oldBook.Review = book.Review;
-
-                return Results.Ok();
-            });
-
-
-            // Delete
-            app.MapDelete("api/books/{id}", (int id, IBookService service) =>
-            {
-                var book = service.Books.Find(x => x.Id == id);
-
-                return service.Books.Remove(book) ? Results.NoContent() : Results.NotFound(id);
-            });
-
+            app.RegisterBookEndpoints();
 
             app.Run();
         }
