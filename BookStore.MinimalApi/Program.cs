8e03954707a261977704c46a823ce3c017651be9
Daniel Thyselius <daniel@mindfulstack.se>
2025-02-18 10:50:58 +0100

    Add rest of endpoints for book

--- BookStore.MinimalApi/Program.cs
+++ BookStore.MinimalApi/Program.cs
@@ -1,5 +1,7 @@
 
 using BookStore.Lib;
+using BookStore.MinimalApi.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.MinimalApi
 {
@@ -15,6 +17,7 @@ namespace BookStore.MinimalApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<IBookService, BookService>();
 
             var app = builder.Build();
 
@@ -30,14 +33,49 @@ namespace BookStore.MinimalApi
             app.UseAuthorization();
 
             // Map endpoints for books
-            // GetAll
-            app.MapGet("/api/books", () => Book.GetSampleData());
+            // Get All
+            app.MapGet("/api/books", (IBookService service) => service.Books);
 
-            // Get
+            // Get by Id
+            app.MapGet("/api/books/{id}", (int id, IBookService service) =>
+            {
+                return service.Books.Find(x => x.Id == id);
+            });
+
             // Post
-            // Put
-            // Delete
+            app.MapPost("api/books", (
+                [FromBody] Book book,
+                [FromServices] IBookService service) =>
+            {
+                var newId = service.Books.Max(x => x.Id) + 1;
+                book.Id = newId;
+                service.Books.Add(book);
+            });
 
+            // Put
+            app.MapPut("api/books/{id}", (int id, Book book, IBookService service) =>
+            {
+                var oldBook = service.Books.Find(x => x.Id == id);
+                if (oldBook is null)
+                    return Results.NotFound(id);
+
+                book.Id = id; // avoid changing Ids, could be handled better
+                oldBook.Title = book.Title;
+                oldBook.Author = book.Author;
+                oldBook.Description = book.Description;
+                oldBook.Review = book.Review;
+
+                return Results.Ok();
+            });
+
+
+            // Delete
+            app.MapDelete("api/books/{id}", (int id, IBookService service) =>
+            {
+                var book = service.Books.Find(x => x.Id == id);
+
+                return service.Books.Remove(book) ? Results.NoContent() : Results.NotFound(id);
+            });
 
 
             app.Run();
---
+++ BookStore.MinimalApi/Services/BookService.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Lib;
+
+namespace BookStore.MinimalApi.Services;
+
+public interface IBookService
+{
+    List<Book> Books { get; set; }
+}
+
+public class BookService : IBookService
+{
+    public List<Book> Books { get; set; }
+    public BookService()
+    {
+        Books = Book.GetSampleData();
+    }
+}
