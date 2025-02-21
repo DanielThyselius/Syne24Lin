af0d99a6b5c349dc3fbadb40342b14e1699ee649
Daniel Thyselius <daniel@mindfulstack.se>
2025-02-21 10:45:45 +0100

    configure swagger

--- BookStore.MinimalApi/Program.cs
+++ BookStore.MinimalApi/Program.cs
@@ -1,6 +1,7 @@
 
 using BookStore.MinimalApi.Endpoints;
 using BookStore.MinimalApi.Services;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace BookStore.MinimalApi
 {
@@ -15,7 +16,24 @@ namespace BookStore.MinimalApi
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new()
+                {
+                    Title = "BookStore API",
+                    Version = "v1.2",
+                    Description = "A simple example book store api",
+                    Contact = new() { Name = "Daniel" }
+                });
+                options.SwaggerDoc("v2", new()
+                {
+                    Title = "BookStore API",
+                    Version = "v2",
+                    Description = "A better example book store api",
+                    Contact = new() { Name = "Daniel" }
+                })
+                ;
+            });
+
             builder.Services.AddSingleton<IBookService, BookService>();
 
 
@@ -25,7 +43,11 @@ namespace BookStore.MinimalApi
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.DocExpansion(DocExpansion.List);
+                    c.DefaultModelsExpandDepth(-1);
+                });
             }
             app.UseStaticFiles();
 
